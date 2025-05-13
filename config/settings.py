b02f331c60a85910cfa6ea786712920b11fccff2
tranthangloi <133758186+tranthangloi@users.noreply.github.com>
2025-05-13 19:15:45 +0700

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-3)l&6g6t_s+di&mt(u08p^&yy@7-l0obv4s4@spi9bu*=p!moi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'futurepathbe-tanbin-production.up.railway.app',  # Domain backend
+    'localhost',  # Cho dev (không cần port)
+]
 
 LOGGING = {
     'version': 1,
